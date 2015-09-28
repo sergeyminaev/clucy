8e898e8767e4a611d2aafdf16eb32f6f39431531
kostafey <kostafey@gmail.com>
2015-09-28 14:11:15 +0400

    Update fork version and url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clucy "0.4.0"
+(defproject org.clojars.kostafey/clucy "0.5.3"
   :description "A Clojure interface to the Lucene search engine"
-  :url "http://github/weavejester/clucy"
+  :url "http://github/kostafey/clucy"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.apache.lucene/lucene-core "5.3.0"]
                  [org.apache.lucene/lucene-queryparser "5.3.0"]
@@ -8,8 +8,5 @@
                  [org.apache.lucene/lucene-highlighter "5.3.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
-  :codox {:src-dir-uri "http://github/weavejester/clucy/blob/master"
-          :src-linenum-anchor-prefix "L"})
+  :profiles {:1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}})
