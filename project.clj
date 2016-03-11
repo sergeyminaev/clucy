c34e542a6db4c5762541537f511cba094f50b5f1
kostafey <kostafey@gmail.com>
2016-03-11 12:35:05 +0400

    Update lucene to 5.5.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.kostafey/clucy "0.5.4.1d"
+(defproject org.clojars.kostafey/clucy "0.5.5.0"
   :description "A Clojure interface to the Lucene search engine"
   :url "http://github/kostafey/clucy"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.lucene/lucene-core "5.4.1"]
-                 [org.apache.lucene/lucene-queryparser "5.4.1"]
-                 [org.apache.lucene/lucene-analyzers-common "5.4.1"]
-                 [org.apache.lucene/lucene-highlighter "5.4.1"]
+                 [org.apache.lucene/lucene-core "5.5.0"]
+                 [org.apache.lucene/lucene-queryparser "5.5.0"]
+                 [org.apache.lucene/lucene-analyzers-common "5.5.0"]
+                 [org.apache.lucene/lucene-highlighter "5.5.0"]
                  [me.raynes/fs "1.4.6"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
