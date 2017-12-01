28f2ec57d0996989b3207ddc99f5c41a3d0e0353
Sergey Minaev <s.minaev@solarsecurity.ru>
2017-12-01 18:28:45 +0300

    Update to lucene 5.5.5, clojure 1.9.0-RC2

--- project.clj
+++ project.clj
@@ -1,15 +1,22 @@
+;; пока artifactory не умеет https
+(require 'cemerick.pomegranate.aether)
+(cemerick.pomegranate.aether/register-wagon-factory!
+ "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
+
 (defproject clucy "0.4.0"
   :description "A Clojure interface to the Lucene search engine"
   :url "http://github/weavejester/clucy"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.apache.lucene/lucene-core "5.3.0"]
-                 [org.apache.lucene/lucene-queryparser "5.3.0"]
-                 [org.apache.lucene/lucene-analyzers-common "5.3.0"]
-                 [org.apache.lucene/lucene-highlighter "5.3.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+
+                 [org.apache.lucene/lucene-core "5.5.5"]
+                 [org.apache.lucene/lucene-queryparser "5.5.5"]
+                 [org.apache.lucene/lucene-analyzers-common "5.5.5"]
+                 [org.apache.lucene/lucene-highlighter "5.5.5"]
+
+                 ;; [org.apache.lucene/lucene-core "7.1.0"]
+                 ;; [org.apache.lucene/lucene-queryparser "7.1.0"]
+                 ;; [org.apache.lucene/lucene-analyzers-common "7.1.0"]
+                 ;; [org.apache.lucene/lucene-highlighter "7.1.0"]
+                 ]
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
-  :codox {:src-dir-uri "http://github/weavejester/clucy/blob/master"
-          :src-linenum-anchor-prefix "L"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
